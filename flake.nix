54d288214df720cb4f2dfdcabbaacaadbc9db61a
Lucas Hoffmann <luc@artsoftandmore.com>
2022-07-16 20:57:05 +0200

    Add neovim versions to shell
    This is useful to run the test suite with different versions of neovim.

--- flake.nix
+++ flake.nix
@@ -3,12 +3,17 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    neovim.url = "github:neovim/neovim?dir=contrib";
-    neovim.inputs.nixpkgs.follows = "nixpkgs";
-    neovim.inputs.flake-utils.follows = "flake-utils";
+    neovim-dev.url = "github:neovim/neovim?dir=contrib";
+    neovim-070.url = "github:neovim/neovim?dir=contrib&ref=v0.7.0";
+    neovim-071.url = "github:neovim/neovim?dir=contrib&ref=v0.7.1";
+    neovim-072.url = "github:neovim/neovim?dir=contrib&ref=v0.7.2";
+    neovim-dev.inputs.nixpkgs.follows = "nixpkgs";
+    neovim-070.inputs.nixpkgs.follows = "nixpkgs";
+    neovim-071.inputs.nixpkgs.follows = "nixpkgs";
+    neovim-072.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, neovim, ... }: {
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs: {
     overlay = final: prev: {
       nvimpager = prev.nvimpager.overrideAttrs (oa: {
         version = "dev";
@@ -19,39 +24,49 @@
     };
   }
   // (let
-    filter = builtins.filter (s: !nixpkgs.lib.strings.hasSuffix "-darwin" s);
+    inherit (nixpkgs.lib.strings) hasPrefix hasSuffix concatStringsSep;
+    inherit (nixpkgs.lib.attrsets) filterAttrs mapAttrs mapAttrsToList;
     inherit (flake-utils.lib) eachSystem defaultSystems;
-  in eachSystem (filter defaultSystems) (system:
+    systems = builtins.filter (s: !hasSuffix "-darwin" s) defaultSystems;
+  in eachSystem systems (system:
   let
-    stable = import nixpkgs { overlays = [ self.overlay ]; inherit system; };
-    nightly = import nixpkgs { overlays = [ neovim.overlay self.overlay ]; inherit system; };
-    mkShell = pkgs: pkgs.mkShell {
+    pkgs = import nixpkgs { overlays = [ self.overlay ]; inherit system; };
+    neovim-packages =
+      mapAttrs (_: f: f.defaultPackage.${system})
+               (filterAttrs (n: _: hasPrefix "neovim-" n) inputs);
+  in rec {
+    packages = {
+      nvimpager = pkgs.nvimpager;
+      neovim-versions = pkgs.stdenv.mkDerivation {
+        pname = "neovim-versions";
+        version = "all";
+        dontUnpack = true;
+        installPhase = ''
+          mkdir -p $out/bin
+          ${concatStringsSep "\n"
+              (mapAttrsToList (n: p: "ln -s ${p}/bin/nvim $out/bin/${n}")
+              neovim-packages)}
+        '';
+      };
+    };
+    defaultPackage = pkgs.nvimpager;
+    apps.nvimpager = flake-utils.lib.mkApp { drv = pkgs.nvimpager; };
+    defaultApp = apps.nvimpager;
+    devShell = pkgs.mkShell {
       inputsFrom = [ pkgs.nvimpager ];
       packages = with pkgs; [
         lua51Packages.luacov
         git
         tmux
         hyperfine
+        self.packages.${system}.neovim-versions
       ];
       shellHook = ''
         # to find nvimpager lua code in the current dir
         export LUA_PATH=./?.lua''${LUA_PATH:+\;}$LUA_PATH
         # fix for different terminals in a pure shell
         export TERM=xterm
-        '';
-      };
-  in rec {
-    packages = {
-      nvimpager = stable.nvimpager;
-      nvimpager-with-nightly-neovim = nightly.nvimpager;
-    };
-    defaultPackage = stable.nvimpager;
-    apps.nvimpager = flake-utils.lib.mkApp { drv = stable.nvimpager; };
-    defaultApp = apps.nvimpager;
-    devShell = devShells.stable;
-    devShells = {
-      stable = mkShell stable;
-      nightly = mkShell nightly;
+      '';
     };
   }));
 }
